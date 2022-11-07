f7390f3fb0bd0352f9b25edcd42976a7cc5dc5e6
A8250092 <stanley.mei08@gmail.com>
2022-11-07 16:58:40 +0800

    Initial commit

---
+++ Two Sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        list=[]
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i]+nums[j]==target:
+                    list.append(i)
+                    list.append(j)
+        return list
+
+
+s=Solution()
+print(s.twoSum([3,6,9,67],70))
+
