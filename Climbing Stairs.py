09ba9a18b763f2b8caa08d476dfa984d8ac5c7d0
stanley1208 <54892792+stanley1208@users.noreply.github.com>
2022-12-31 16:04:39 +0800

    update

---
+++ Climbing Stairs.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def climbStairs(self, n):
+        if n==1:
+            return 1
+        if n==2:
+            return 2
+
+        return self.climbStairs(n-1)+self.climbStairs(n-2)
+
+
+
+
+
+
+sol=Solution().climbStairs(3)
+print(sol)
+
+
