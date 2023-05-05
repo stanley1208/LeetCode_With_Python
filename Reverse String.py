00a4e99bc04c0a040f94079ca1dcb1f660b38b59
A8250092 <stanley.mei08@gmail.com>
2023-05-05 17:46:29 +0800

    update

---
+++ Reverse String.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        s=s.reverse()
+
+
+
+
+sol=Solution().reverseString(["H","a","n","n","a","h"])
