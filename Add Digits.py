8fa9afa422f15d36a0e84b109c9cbe7b6c8e3af4
A8250092 <stanley.mei08@gmail.com>
2023-05-26 17:57:48 +0800

    update

---
+++ Add Digits.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def addDigits(self, num):
+        """
+        :type num: int
+        :rtype: int
+        """
+
+        sum=0
+        string=str(num)
+        for i in string:
+            i=int(i)
+            sum+=i
+
+        if len(str(sum)) == 1:
+            return sum
+        else:
+            return self.addDigits(sum)
+
+
+
+
+
+
+sol=Solution().addDigits(0)
+print(sol)
