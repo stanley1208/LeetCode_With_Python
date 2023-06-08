12618bd2f9300c0421b1e0074cfc47e13346f010
A8250092 <stanley.mei08@gmail.com>
2023-06-09 06:50:16 +0800

    update

---
+++ Count Negative Numbers in a Sorted Matrix.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def countNegatives(self, grid):
+        """
+        :type grid: List[List[int]]
+        :rtype: int
+        """
+        count=0
+
+        for i in grid:
+            for j in i:
+                if j<0:
+                    count+=1
+
+        return count
+
+sol=Solution().countNegatives([[3,2],[1,0]])
+print(sol)
