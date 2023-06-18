a2f13f8b076331ebcda46b2cd2442b3e8425ba6b
A8250092 <stanley.mei08@gmail.com>
2023-06-18 10:47:39 +0800

    update

---
+++ Add Two Integers.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def sum(self, num1, num2):
+        """
+        :type num1: int
+        :type num2: int
+        :rtype: int
+        """
+        return num1+num2
+
+sol=Solution().sum(-10,4)
+print(sol)
