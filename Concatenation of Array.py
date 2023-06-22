16575bffb932c4eb72aab7465505ffcdeeeb353a
A8250092 <stanley.mei08@gmail.com>
2023-06-23 06:54:13 +0800

    update

---
+++ Concatenation of Array.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        return nums*2
+
+sol=Solution().getConcatenation([1,3,2,1])
+print(sol)
